6ebbec47aac7d3f4da51b23984394f096d57cc40
Moritz Widmayer <moritz.widmayer@andrena.de>
2025-04-07 00:24:43 +0200

    change ta

--- ta_utility.py
+++ ta_utility.py
@@ -15,18 +15,19 @@ def get_macd(close):
     return macd.macd().tolist(), macd.macd_signal().tolist(), macd.macd_diff().tolist()
 
 
-def get_sma(close, window=220):
-    sma_220 = SMAIndicator(pd.Series(close), window=window).sma_indicator()
-    return sma_220.tolist()
+def get_sma(close, window=200):
+    sma = SMAIndicator(pd.Series(close), window=window).sma_indicator()
+    return sma.tolist()
 
 
 def get_technicals(close):
+    bullish = True
     rsi, rsi_sma = get_rsi(close)
     if rsi[-1] > 70.0 or rsi_sma[-1] > 70.0:
-        return None
+        bullish = False
 
     macd, macd_signal, macd_diff = get_macd(close)
-    if macd_diff[-1] < 0.0:
-        return None
+    if macd_diff[-1] < 0.0 or macd_diff[-1] < macd_diff[-2]:
+        bullish = False
 
-    return rsi, rsi_sma, macd, macd_signal, macd_diff
+    return bullish, rsi, rsi_sma, macd, macd_signal, macd_diff
